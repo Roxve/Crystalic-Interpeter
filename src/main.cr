7e5245857e59660fdfcbfd31466d12e313699902
Atonix0 <116169309+Atonix0@users.noreply.github.com>
2023-09-03 19:51:55 +0000

    added exiting...

--- src/main.cr
+++ src/main.cr
@@ -2,12 +2,15 @@ require "./tokenizer.cr";
 require "./parser/parser.cr";
 require "./interpeter/main.cr"
 require "./parser/AST.cr";
-puts "Welcome to The Crystalic*Interpeter"
+puts "Welcome to The Crystalic*Interpeter\nType 'exit' to exit!"
 
 while true 
   print(">>");
   STDOUT.flush
   code : String = gets || "0"
+  if code.upcase.includes? "EXIT"
+    exit
+  end
   tokenizer = Tokenizer.new(code)
   tokenized = tokenizer.tokenize
   parser = Parser.new(tokenized);
